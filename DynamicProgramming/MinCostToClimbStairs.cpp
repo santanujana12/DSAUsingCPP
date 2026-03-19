c59689b889edcaebc0ea351b541ff254f5d46692
santanujana12 <santanu.jana@aol.com>
2026-03-20 01:33:32 +0530

    Refactor minCostClimbingStairs implementation and add iterative approach

--- DynamicProgramming/MinCostToClimbStairs.cpp
+++ DynamicProgramming/MinCostToClimbStairs.cpp
@@ -1,42 +1,63 @@
 // Input and print written in the header file
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 #define endl '\n'
 #define ll long long int
 #define ld long long double
 using namespace std;
 
-unordered_map<int,int>isPresent;
+unordered_map<int, int> isPresent;
 
-int findMinCost(vector<int>&cost,int n){
-	if(n==0){
+// Iterative approach
+int minCostClimbingStairs(vector<int> &cost)
+{
+	int n = cost.size();
+	vector<int> arr(n + 1);
+	arr.push_back(0);
+	arr.push_back(0);
+	for (int i = 2; i <= n; i++)
+	{
+		arr[i] = min(arr[i - 1] + cost[i - 1], arr[i - 2] + cost[i - 2]);
+	}
+	return arr[n];
+}
+
+int findMinCost(vector<int> &cost, int n)
+{
+	if (n == 0)
+	{
 		return cost[0];
 	}
-	if(n==1){
+	if (n == 1)
+	{
 		return cost[1];
 	}
-	if(isPresent.find(n) != isPresent.end()){
-        return isPresent[n];
-    }
-	int oneStep = cost[n]+findMinCost(cost,n-1);
-	int twoStep = cost[n]+findMinCost(cost,n-2);
-	isPresent[n] = min(oneStep,twoStep);
+	if (isPresent.find(n) != isPresent.end())
+	{
+		return isPresent[n];
+	}
+	int oneStep = cost[n] + findMinCost(cost, n - 1);
+	int twoStep = cost[n] + findMinCost(cost, n - 2);
+	isPresent[n] = min(oneStep, twoStep);
 	return isPresent[n];
 }
 
-int minCostClimbingStairs(vector<int>& cost) {
-    int n = cost.size();
-    return min(findMinCost(cost,n-1),findMinCost(cost,n-2));
+int minCostClimbingStairs(vector<int> &cost)
+{
+	int n = cost.size();
+	return min(findMinCost(cost, n - 1), findMinCost(cost, n - 2));
 }
 
-int main(){
+int main()
+{
 	int n;
-	cin>>n;
-	vector<int>arr;
-	for(int i=0;i<n;i++){
+	cin >> n;
+	vector<int> arr;
+	for (int i = 0; i < n; i++)
+	{
 		int a;
-		cin>>a;
+		cin >> a;
 		arr.push_back(a);
 	}
-	
-	cout<<minCostClimbingStairs(arr)<<endl;
+
+	cout << minCostClimbingStairs(arr) << endl;
 }
