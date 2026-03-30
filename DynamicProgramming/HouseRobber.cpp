1914b5f61d4710cfbf4e304443b3e1c5046eba4a
santanu.jana@aol.com <santanu.jana@aol.com>
2026-03-31 01:14:42 +0530

    Add implementation for House Robber problem using recursion

---
+++ DynamicProgramming/HouseRobber.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int maximizeRob(vector<int> &nums, int i)
+{
+    if (i >= nums.size())
+    {
+        return 0;
+    }
+    int rob = nums[i] + maximizeRob(nums, i + 2);
+    int skip = maximizeRob(nums, i + 1);
+
+    return max(rob, skip);
+}
+
+int rob(vector<int> &nums)
+{
+    return maximizeRob(nums, 0);
+}
+
+int main()
+{
+    vector<int> nums = {2, 7, 9, 3, 1};
+    cout << rob(nums) << endl; // Output: 4 (rob 1 and 3)
+}
