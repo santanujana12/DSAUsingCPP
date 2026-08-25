7cf3bb2ae935a2605471c68e0a335fdca67069e0
santanujana12 <santanu.jana@aol.com>
2026-08-26 01:38:33 +0530

    Refactor to use 'using namespace std' and remove std:: prefix for vector and cout

--- BinarySearch/LogicBuilding/FirstAndLastOccurence.cpp
+++ BinarySearch/LogicBuilding/FirstAndLastOccurence.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 #include <vector>
+using namespace std;
 
-int findFirst(std::vector<int> &arr, int target) {
+int findFirst(vector<int> &arr, int target) {
   int res = -1;
   int start = 0, end = arr.size() - 1;
   while (start <= end) {
@@ -19,7 +20,7 @@ int findFirst(std::vector<int> &arr, int target) {
   return res;
 }
 
-int findLast(std::vector<int> &arr, int target) {
+int findLast(vector<int> &arr, int target) {
   int res = -1;
   int start = 0, end = arr.size() - 1;
   while (start <= end) {
@@ -37,8 +38,8 @@ int findLast(std::vector<int> &arr, int target) {
   return res;
 }
 
-std::vector<int> searchRange(std::vector<int> &arr, int target) {
-  std::vector<int> result(2);
+vector<int> searchRange(vector<int> &arr, int target) {
+  vector<int> result(2);
 
   result[0] = findFirst(arr, target);
   result[1] = findLast(arr, target);
@@ -47,12 +48,12 @@ std::vector<int> searchRange(std::vector<int> &arr, int target) {
 }
 
 int main() {
-  std::vector<int> arr = {5, 7, 7, 8, 8, 10};
+  vector<int> arr = {5, 7, 7, 8, 8, 10};
   int target = 8;
-  std::vector<int> result = searchRange(arr, target);
+  vector<int> result = searchRange(arr, target);
   for (int i : result) {
-    std::cout << i << " ";
+    cout << i << " ";
   }
-  std::cout << std::endl;
+  cout << endl;
   return 0;
 }
